75165555f7417684ee42f0a53a9b83768a8fb513
adyze <115144204+adamurbanek@users.noreply.github.com>
2023-05-11 20:12:36 +0200

    UZ TO JEZDI ASPON NA TLACITKA!!!!

--- main.ts
+++ main.ts
@@ -1,95 +1,55 @@
-// //servo S1
-// //levý motor M1
-// //pravý motor M4
+// //funkce pro motory
+// function car_motor(lw: number = 0, rw: number = 0) {
+//     const ul = Math.map(lw, -100, 100, -255, 255)
+//     const ur = Math.map(rw, -100, 100, -255, 255)
+//     PCAmotor.MotorRun(PCAmotor.Motors.M1, -ul)
+//     PCAmotor.MotorRun(PCAmotor.Motors.M4, ur)
+// }
 
-// /*senzory 
-// center:P15
-// pravý senzor :P13
-// levý senzor: P14*/
-
-// /*
-// P16 4x RGB
-// ECHO oranžová P1
-// TRIGR bílá P2*/
-// let leftIdx = 0
-// let rightInx = 0
-// const speeds = [0, 30, 50, 70, 85, 100]
-
-// input.onButtonPressed(Button.A, function () {
-//     PCAmotor.MotorRun(PCAmotor.Motors.M1,
-//         speeds[++leftIdx % speeds.length])
-//     basic.showNumber(speeds[leftIdx])
-//     basic.clearScreen()
-// })
-
-// input.onButtonPressed(Button.B, function () {
-//     PCAmotor.MotorRun(PCAmotor.Motors.M4,
-//         speeds[++rightInx % speeds.length])
-//     basic.showNumber(speeds[rightInx])
-//     basic.clearScreen()
-// })
-// input.onButtonPressed(Button.AB, function () {
-//     PCAmotor.MotorStopAll()
-//     leftIdx = rightInx = 0
-// })
-
-
-
-//funkce pro motory
-function car_motor(lw: number = 0, rw: number = 0) {
-    const ul = Math.map(lw, -100, 100, -255, 255)
-    const ur = Math.map(rw, -100, 100, -255, 255)
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, -ul)
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, ur)
-}
-
+// //automatizovana jizda
 // function dopredu() {
 //     PCAmotor.MotorRun(PCAmotor.Motors. M1 && M4)
 //     basic.pause(4000);
 //     PCAmotor.MotorStopAll;
 // }
 
+
+
 //jizda
-radio.setGroup(22);
-radio.setFrequencyBand(6);
+radio.setGroup(54)
+radio.setFrequencyBand(7)
 
-const speeds = [0, 50, 100, 255];
-let levyIndex = 0;
-let pravyIndex = 0;
-let x = 0;
-let y = 0;
-let a = 0;
-let b = 0;
-let l = 0;
+let btnA = 0
+let btnB = 0
+let logo = 0
 
-radio.onReceivedString(function(receivedString: string) {
-    let array = [];
-
-    array.push(recievedString.charCodeAt(0)) // znak 1 - dimenze x
-    array.push(recievedString.charCodeAt(1)) // znak 2 - dimenze y
+radio.onReceivedString(function (receivedString: string) {
+    let arr = []
+    arr.push(receivedString.charCodeAt(0)) // znak 1 - dimenze x
+    arr.push(receivedString.charCodeAt(1)) // znak 2 - dimenze y
 
     for (let i = 2; i <= 7; i++) {
-        array.push(parseInt(recievedString.charAt(i)));
+        arr.push(parseInt(receivedString.charAt(i)));
     }
-}
+    btnA = arr[2]
+    btnB = arr[3]
+    logo = arr[4]
+})
 
-basic.forever(function() {
-    if (array[4] == 1) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, speeds[2])
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, speeds[2])
-    }
-
-    if (array[2] == 1) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, speeds[2])
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, speeds[0])
+basic.forever(function () {
+    if (logo) {
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, -250)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, -250)
     } else {
-
-    }
-
-    if (array[3] == 1) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, speeds[2])
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, speeds[0])
-    } else {
-
+        if (btnA) {
+            PCAmotor.MotorRun(PCAmotor.Motors.M1, 250)
+        } else {
+            PCAmotor.MotorStop(PCAmotor.Motors.M1)
+        }
+        if (btnB) {
+            PCAmotor.MotorRun(PCAmotor.Motors.M4, 250)
+        } else {
+            PCAmotor.MotorStop(PCAmotor.Motors.M4)
+        }
     }
 })
