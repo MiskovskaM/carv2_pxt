65ee96d1b0a814fa230928473235b7dbbf92f194
adyze <115144204+adamurbanek@users.noreply.github.com>
2023-05-11 20:23:31 +0200

    středníky !!!! (nekod neumi :D)

--- main.ts
+++ main.ts
@@ -16,40 +16,46 @@
 
 
 //jizda
-radio.setGroup(54)
-radio.setFrequencyBand(7)
+radio.setGroup(54);
+radio.setFrequencyBand(7);
 
-let btnA = 0
-let btnB = 0
-let logo = 0
+let btnA = 0;
+let btnB = 0;
+let logo = 0;
 
 radio.onReceivedString(function (receivedString: string) {
-    let arr = []
-    arr.push(receivedString.charCodeAt(0)) // znak 1 - dimenze x
-    arr.push(receivedString.charCodeAt(1)) // znak 2 - dimenze y
+    let arr = [];
+    arr.push(receivedString.charCodeAt(0)); // znak 1 - dimenze x
+    arr.push(receivedString.charCodeAt(1)); // znak 2 - dimenze y
+    arr.push(receivedString.charCodeAt(2)); // znak 3 - tlacitko a
+    arr.push(receivedString.charCodeAt(3)); // znak 4 - tlacitko b
+    arr.push(receivedString.charCodeAt(4)); // znak 5 - logo
+    arr.push(receivedString.charCodeAt(5)); // znak 6 - pin P0
+    arr.push(receivedString.charCodeAt(6)); // znak 7 - pin P1
+    arr.push(receivedString.charCodeAt(7)); // znak 8 - pin P2
 
     for (let i = 2; i <= 7; i++) {
         arr.push(parseInt(receivedString.charAt(i)));
     }
-    btnA = arr[2]
-    btnB = arr[3]
-    logo = arr[4]
+    btnA = arr[2];
+    btnB = arr[3];
+    logo = arr[4];
 })
 
 basic.forever(function () {
     if (logo) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, -250)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, -250)
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, -250);
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, -250);
     } else {
         if (btnA) {
-            PCAmotor.MotorRun(PCAmotor.Motors.M1, 250)
+            PCAmotor.MotorRun(PCAmotor.Motors.M1, 250);
         } else {
-            PCAmotor.MotorStop(PCAmotor.Motors.M1)
+            PCAmotor.MotorStop(PCAmotor.Motors.M1);
         }
         if (btnB) {
-            PCAmotor.MotorRun(PCAmotor.Motors.M4, 250)
+            PCAmotor.MotorRun(PCAmotor.Motors.M4, 250);
         } else {
-            PCAmotor.MotorStop(PCAmotor.Motors.M4)
+            PCAmotor.MotorStop(PCAmotor.Motors.M4);
         }
     }
 })
