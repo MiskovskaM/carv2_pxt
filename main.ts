15248f2199117021dbf6d0521fb25fe84d9117e9
adyze <115144204+adamurbanek@users.noreply.github.com>
2023-05-19 18:00:18 +0200

    dfghfth fghjfghj

--- main.ts
+++ main.ts
@@ -1,37 +1,73 @@
 //jizda
-radio.setGroup(54)
-radio.setFrequencyBand(7)
+radio.setGroup(54);
+radio.setFrequencyBand(7);
 
-let x = 0
-let y = 0
+let x = 0;
+let y = 0;
 
 radio.onReceivedString(function (receivedString: string) {
-    let arr = []
-    arr.push(receivedString.charCodeAt(0)) // znak 1 - dimenze x
-    arr.push(receivedString.charCodeAt(1)) // znak 2 - dimenze y
+    let arr = [];
+    arr.push(receivedString.charCodeAt(0)); // znak 1 - dimenze x
+    arr.push(receivedString.charCodeAt(1)); // znak 2 - dimenze y
 
     for (let i = 2; i <= 7; i++) {
         arr.push(parseInt(receivedString.charAt(i)));
     }
-    x = arr[0]
-    y = arr[1]
-})
+
+    x = Math.round((arr[0] / 255) * 2048 - 1024);
+    y = Math.round((arr[1] / 255) * 2048 - 1024);
+});
 
 basic.forever(function () {
-    if (y > 200) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, 150)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, -150)
-    } else if (y < -200) { 
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, -150)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, 150)
-    } else if (x > 200) { 
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, 150)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, 150)
-    } else if (x < -200) {
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, -150)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, -150)
+    if (y < -50) {
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, 150);
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, 150);
+    } else if (y > 50) {
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, -150);
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, -150);
+    } else if (x < -50) {
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, -150);
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, 150);
+
+    } else if (x > 50) {
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, 150);
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, -150);
     } else {
-        PCAmotor.MotorStop(PCAmotor.Motors.M1)
-        PCAmotor.MotorStop(PCAmotor.Motors.M4)
+        PCAmotor.MotorStop(PCAmotor.Motors.M1);
+        PCAmotor.MotorStop(PCAmotor.Motors.M4);
     }
 })
+
+// basic.forever(function () {
+//     if (x > 100) {
+//         PCAmotor.MotorRun(PCAmotor.Motors.M1, 150);
+//         PCAmotor.MotorRun(PCAmotor.Motors.M4, -150);
+//     } else if (x < -100) { 
+//         PCAmotor.MotorRun(PCAmotor.Motors.M1, -150);
+//         PCAmotor.MotorRun(PCAmotor.Motors.M4, 150);
+//     } else if (y < -100) { 
+//         PCAmotor.MotorRun(PCAmotor.Motors.M1, 150);
+//         PCAmotor.MotorRun(PCAmotor.Motors.M4, 150);
+//     } else if (y > 100) {
+//         PCAmotor.MotorRun(PCAmotor.Motors.M1, -150);
+//         PCAmotor.MotorRun(PCAmotor.Motors.M4, -150);
+//     } else {
+//         // PCAmotor.MotorStop(PCAmotor.Motors.M1);
+//         // PCAmotor.MotorStop(PCAmotor.Motors.M4);
+//         PCAmotor.MotorRun(PCAmotor.Motors.M1, 0);
+//         PCAmotor.MotorRun(PCAmotor.Motors.M4, 0);
+//     }
+// })
+
+// basic.forever(function() {
+//     if (y > 0) {
+//         PCAmotor.MotorRun(PCAmotor.Motors.M1, 100);
+//         PCAmotor.MotorRun(PCAmotor.Motors.M4, 100);
+//     } else if (y < 0) {
+//         PCAmotor.MotorRun(PCAmotor.Motors.M1, -100);
+//         PCAmotor.MotorRun(PCAmotor.Motors.M4, -100);
+//     } else {
+//         PCAmotor.MotorStop(PCAmotor.Motors.M1);
+//         PCAmotor.MotorStop(PCAmotor.Motors.M4);
+//     }
+// })
